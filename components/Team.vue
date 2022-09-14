e6c8b72f76be2da56a5ae7b73f728fd4aef3e081
Lo Van Long <longlv@v-auto.com.vn>
2022-09-14 18:34:09 +0700

    generate

--- components/Team.vue
+++ components/Team.vue
@@ -5,21 +5,21 @@
             <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Nhân sự Bưu điện huyện Mê Linh</h2>
             <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">Thông tin liên hệ của các nhân viên bưu điện Huyện Mê Linh</p>
         </div> 
-        <div class="grid gap-8 lg:gap-16 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-            <div class="text-center text-gray-500 dark:text-gray-400">
-                <img class="mx-auto mb-4 w-36 h-36 rounded-full" src="https://buudienxatulap.ga/images/gui-hang-qua-buu-dien.jpg" alt="Bonnie Avatar">
+        <div class="flex overflow-x-auto space-x-16">
+            <div v-for="{soDienThoai,hoTen,chucVu,imageUrl} in nhanSus" :key="soDienThoai" class="text-center text-gray-500 dark:text-gray-400">
+                <img class="mx-auto mb-4 rounded-full" :src="imageUrl" alt="Bonnie Avatar">
                 <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                    <a href="#">Lỗ Văn Tỉnh</a>
+                    <a href="#">{{hoTen}}</a>
                 </h3>
-                <p>Kinh doanh 1</p>
+                <p>{{chucVu}}</p>
                 <ul class="flex justify-center mt-4 space-x-4">
                     <li>
-                        <a href="#" class="text-[#39569c] hover:text-gray-900 dark:hover:text-white">
-                            <!-- <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" /></svg> -->
+                        <a :href="`tel:${soDienThoai}`" class="text-[#39569c] hover:text-gray-900 dark:hover:text-white  inline-flex items-center">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
     <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"  clip-rule="evenodd" />
   </svg>
-                        </a>
+                        
+                        {{soDienThoai}}</a>
                     </li>
                     
                 </ul>
@@ -29,3 +29,46 @@
     </div>
   </section>
 </template>
+
+<script>
+export default {
+    data () {
+        return {
+            nhanSus: [
+                {
+                    soDienThoai:'0978333963',
+                    hoTen:"Lỗ Văn Tỉnh",
+                    chucVu:"Nhân viên kinh doanh",
+                    imageUrl: "https://buudienxatulap.ga/images/gui-hang-qua-buu-dien.jpg"
+                },  
+                {
+                    soDienThoai:'0978333963',
+                    hoTen:"Lỗ Văn Tỉnh",
+                    chucVu:"Nhân viên kinh doanh",
+                    imageUrl: "https://buudienxatulap.ga/images/gui-hang-qua-buu-dien.jpg"
+                },  
+                {
+                    soDienThoai:'0978333963',
+                    hoTen:"Lỗ Văn Tỉnh",
+                    chucVu:"Nhân viên kinh doanh",
+                    imageUrl: "https://buudienxatulap.ga/images/gui-hang-qua-buu-dien.jpg"
+                },  
+                {
+                    soDienThoai:'0978333963',
+                    hoTen:"Lỗ Văn Tỉnh",
+                    chucVu:"Nhân viên kinh doanh",
+                    imageUrl: "https://buudienxatulap.ga/images/gui-hang-qua-buu-dien.jpg"
+                },  
+                {
+                    soDienThoai:'0978333963',
+                    hoTen:"Lỗ Văn Tỉnh",
+                    chucVu:"Nhân viên kinh doanh",
+                    imageUrl: "https://buudienxatulap.ga/images/gui-hang-qua-buu-dien.jpg"
+                },  
+ 
+                
+            ]
+        }
+    }
+}
+</script>
