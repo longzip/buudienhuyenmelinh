5f607ab077dbc5eba96ad54aa2a2b716d7272225
Lo Van Long <longlv@v-auto.com.vn>
2022-09-15 06:48:45 +0700

    generate

--- layouts/default.vue
+++ layouts/default.vue
@@ -48,11 +48,11 @@
     <!-- <Footer /> -->
     <!-- <div class="zalo-chat-widget" data-oaid="907870636305349915" data-welcome-message="Rất vui khi được hỗ trợ bạn!" data-autopopup="0" data-width="" data-height=""></div> -->
         <!-- Messenger Plugin chat Code -->
-    <div id="fb-root"></div>
+    <!-- <div id="fb-root"></div> -->
 
     <!-- Your Plugin chat code -->
-    <div id="fb-customer-chat" class="fb-customerchat">
-    </div>
+    <!-- <div id="fb-customer-chat" class="fb-customerchat">
+    </div> -->
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-QJFM4GBK14"></script>
     <script>
@@ -62,8 +62,8 @@
 
         gtag('config', 'G-QJFM4GBK14');
     </script>
-    <script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/03e33e3cdd5e422de02a01d6e/a349bd51a875ff4001de83a79.js");</script>
-    <script src="https://sp.zalo.me/plugins/sdk.js"></script>
+    <!-- <script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/03e33e3cdd5e422de02a01d6e/a349bd51a875ff4001de83a79.js");</script> -->
+    <!-- <script src="https://sp.zalo.me/plugins/sdk.js"></script> -->
   </div>
 </template>
 <script>
