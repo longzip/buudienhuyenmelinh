2344b245e7575def7868c517d7c17c9b152e1efe
Lo Van Long <longlv@v-auto.com.vn>
2022-09-28 16:51:45 +0700

    generate

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
 
 <div id="default-carousel" class="relative" data-carousel="static">
     <!-- Carousel wrapper -->
-    <div class="relative h-80 overflow-hidden rounded-lg md:h-96">
+    <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
          <!-- Item 1 -->
         <div class="hidden duration-700 ease-in-out" data-carousel-item>
             <img src="/images/buu-dien-huyen-me-linh-2.png" class="invisible md:visible absolute block w-full -translate-x-1/2 -translate-y-1/2 " alt="...">
